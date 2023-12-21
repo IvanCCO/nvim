d75e3586084cb07b34e229324b7db3c3149bfb69
IvanCCO <ivanmedeiros0903@outlook.com>
2023-12-21 00:01:00 -0300

    teste

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,14 +19,14 @@ keymap.set("n", "<leader>to", ":BufferLineCloseOthers<cr>", opts)
 -- Adding a new tab (file)
 keymap.set("n", "<leader>tn", ":tabnew<cr>", opts)
 -- Select all file
-keymap.set("n", "ya", "<esc>ggVG", opts)
+keymap.set("n", "va", "<esc>ggVG", opts)
 
 -- Go to the begin and end of a line using control e-a like on terminal
 keymap.set("i", "<C-e>", "<end>", opts)
 keymap.set("i", "<C-a>", "<home>", opts)
 
 -- Add a line bellow without put cursor down
-keymap.set("n", "<space>o", "printf('m`%so<ESC>``', v:count1)", opts)
+keymap.set("n", "<leader>o", "printf('m`%so<ESC>``', v:count2)", opts)
 
 --  Transform word on uppercase
 keymap.set("n", "<leader>up", "<Esc>viwUea<Esc>", opts)
