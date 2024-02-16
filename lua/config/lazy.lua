da6371f30cd0f9e255c273b1bded89e4033dd347
IvanCCO <ivanmedeiros0903@outlook.com>
2024-02-16 22:08:26 +0000

    tirando li

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,8 +15,8 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
