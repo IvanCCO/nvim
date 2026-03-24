c062040d41883f5ae19499b8606d04985f5e9709
IvanCCO <ivanmedeiros0903@outlook.com>
2026-03-24 15:20:54 -0400

    AI Config

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,35 +1,63 @@
 return {
-	{
-		"milanglacier/minuet-ai.nvim",
-		event = "InsertEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("minuet").setup({
-				provider = "gemini",
-				provider_options = {
-					gemini = {
-						model = "gemini-2.0-flash",
-						optional = {
-							generationConfig = {
-								maxOutputTokens = 256,
-								thinkingConfig = {
-									thinkingBudget = 0,
-								},
-							},
-						},
-					},
-				},
-				virtualtext = {
-					auto_trigger_ft = { "*" },
-					keymap = {
-						accept = "<A-a>",
-						accept_line = "<A-l>",
-						next = "<A-]>",
-						prev = "<A-[>",
-						dismiss = "<A-e>",
-					},
-				},
-			})
-		end,
-	},
+  {
+    "milanglacier/minuet-ai.nvim",
+    event = "InsertEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("minuet").setup({
+        provider = "openai_compatible",
+        request_timeout = 2.5,
+        throttle = 1500,
+        debounce = 600,
+        provider_options = {
+          openai_compatible = {
+            api_key = "OPENROUTER_API_KEY",
+            end_point = "https://openrouter.ai/api/v1/chat/completions",
+            model = "moonshotai/kimi-k2",
+            name = "Openrouter",
+            stream = true,
+            optional = {
+              max_tokens = 256,
+              top_p = 0.9,
+              provider = {
+                sort = "throughput",
+              },
+            },
+          },
+        },
+        virtualtext = {
+          auto_trigger_ft = { "*" },
+          keymap = {
+            accept = "<A-a>",
+            accept_line = "<A-l>",
+            next = "<A-]>",
+            prev = "<A-[>",
+            dismiss = "<A-e>",
+          },
+        },
+      })
+    end,
+  },
+
+  -- blink.cmp integration
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "milanglacier/minuet-ai.nvim" },
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      opts.sources.default = vim.list_extend(opts.sources.default or {}, { "minuet" })
+      opts.sources.providers = opts.sources.providers or {}
+      opts.sources.providers.minuet = {
+        name = "minuet",
+        module = "minuet.blink",
+        score_offset = 8,
+      }
+      -- Provider icon for completion menu
+      opts.appearance = opts.appearance or {}
+      opts.appearance.kind_icons = vim.tbl_extend("force", opts.appearance.kind_icons or {}, {
+        Openrouter = "󱂇",
+      })
+    end,
+  },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,116 +1,118 @@
 return {
-	{
-		"rcarriga/nvim-notify",
-		opts = {
-			timeout = 5000,
-		},
-	},
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 8000,
+      stages = "static",
+      render = "wrapped-compact",
+    },
+  },
 
-	-- animations
-	{
-		"nvim-mini/mini.animate",
-		event = "VeryLazy",
-		opts = function(_, opts)
-			opts.scroll = {
-				enable = false,
-			}
-		end,
-	},
+  -- animations
+  {
+    "nvim-mini/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
 
-	-- buffer line
-	{
-		"akinsho/bufferline.nvim",
-		event = "VeryLazy",
-		keys = {
-			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-		},
-		opts = {
-			options = {
-				mode = "tabs",
-				-- separator_style = "slant",
-				show_buffer_close_icons = true,
-				show_close_icon = true,
-			},
-		},
-	},
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        -- separator_style = "slant",
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+      },
+    },
+  },
 
-	-- filename
-	{
-		"b0o/incline.nvim",
-		dependencies = { "craftzdog/solarized-osaka.nvim" },
-		event = "BufReadPre",
-		priority = 1200,
-		config = function()
-			local colors = require("solarized-osaka.colors").setup()
-			require("incline").setup({
-				highlight = {
-					groups = {
-						InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
-						InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
-					},
-				},
-				window = { margin = { vertical = 0, horizontal = 1 } },
-				hide = {
-					cursorline = true,
-				},
-				render = function(props)
-					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-					if vim.bo[props.buf].modified then
-						filename = "[+] " .. filename
-					end
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
 
-					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-					return { { icon, guifg = color }, { " " }, { filename } }
-				end,
-			})
-		end,
-	},
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
 
-	-- statusline
-	{
-		"nvim-lualine/lualine.nvim",
-		opts = function(_, opts)
-			local LazyVim = require("lazyvim.util")
-			opts.sections.lualine_c[4] = {
-				LazyVim.lualine.pretty_path({
-					length = 0,
-					relative = "cwd",
-					modified_hl = "MatchParen",
-					directory_hl = "",
-					filename_hl = "Bold",
-					modified_sign = "",
-					readonly_icon = " 󰌾 ",
-				}),
-			}
-		end,
-	},
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local LazyVim = require("lazyvim.util")
+      opts.sections.lualine_c[4] = {
+        LazyVim.lualine.pretty_path({
+          length = 0,
+          relative = "cwd",
+          modified_hl = "MatchParen",
+          directory_hl = "",
+          filename_hl = "Bold",
+          modified_sign = "",
+          readonly_icon = " 󰌾 ",
+        }),
+      }
+    end,
+  },
 
-	{
-		"folke/zen-mode.nvim",
-		cmd = "ZenMode",
-		opts = {
-			plugins = {
-				gitsigns = true,
-				tmux = true,
-				kitty = { enabled = false, font = "+2" },
-			},
-		},
-		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-	},
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+        kitty = { enabled = false, font = "+2" },
+      },
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
 
-	{
-		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		opts = function(_, opts)
-			local logo = [[
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
       ____ ____ ____ ____ ____ ____
       ||R |||E |||T |||U |||R |||N ||
       ||__|||__|||__|||__|||__|||__||
       |/__\|/__\|/__\|/__\|/__\|/__\|
     ]]
-			logo = string.rep("\n", 8) .. logo .. "\n\n"
-			opts.config.header = vim.split(logo, "\n")
-		end,
-	},
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
 }
